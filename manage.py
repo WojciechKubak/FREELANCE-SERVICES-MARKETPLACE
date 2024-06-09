d1c97200be8446667338c731703b98945dbdafa2
WojciechKubak <wojciechkubak99@gmail.com>
2024-06-09 09:35:38 +0200

    FIX: app runup for manager.py

--- manage.py
+++ manage.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
+from app.settings.base import DEBUG
 import os
 import sys
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
+    os.environ.setdefault(
+        "DJANGO_SETTINGS_MODULE",
+        "app.settings.development" if DEBUG else "app.settings.production",
+    )
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:

756fc7c98359775baa8d676e2c34d7f348294272
WojciechKubak <wojciechkubak99@gmail.com>
2024-06-09 19:40:06 +0200

    UPDATE: formatting files

--- custom_auth/migrations/0001_initial.py
+++ custom_auth/migrations/0001_initial.py
@@ -8,24 +8,36 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='User',
+            name="User",
             fields=[
-                ('password', models.CharField(max_length=128, verbose_name='password')),
-                ('last_login', models.DateTimeField(blank=True, null=True, verbose_name='last login')),
-                ('id', models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False)),
-                ('username', models.CharField(max_length=255, unique=True)),
-                ('email', models.EmailField(max_length=255, unique=True)),
-                ('role', models.CharField(max_length=50)),
-                ('is_active', models.BooleanField(default=False)),
-                ('is_admin', models.BooleanField(default=False)),
+                ("password", models.CharField(max_length=128, verbose_name="password")),
+                (
+                    "last_login",
+                    models.DateTimeField(
+                        blank=True, null=True, verbose_name="last login"
+                    ),
+                ),
+                (
+                    "id",
+                    models.UUIDField(
+                        default=uuid.uuid4,
+                        editable=False,
+                        primary_key=True,
+                        serialize=False,
+                    ),
+                ),
+                ("username", models.CharField(max_length=255, unique=True)),
+                ("email", models.EmailField(max_length=255, unique=True)),
+                ("role", models.CharField(max_length=50)),
+                ("is_active", models.BooleanField(default=False)),
+                ("is_admin", models.BooleanField(default=False)),
             ],
             options={
-                'db_table': 'users',
+                "db_table": "users",
             },
         ),
     ]
--- custom_auth/serializers.py
+++ custom_auth/serializers.py
@@ -1,8 +1,5 @@
 from rest_framework import serializers
-from rest_framework_jwt.settings import api_settings
 from .models import User
-from django.contrib.auth import authenticate
-from django.contrib.auth.models import update_last_login
 from typing import Any
 
 

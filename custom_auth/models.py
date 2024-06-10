1c6b6989c350a64d5a0fd70be2839e4aca96be1b
WojciechKubak <wojciechkubak99@gmail.com>
2024-06-10 19:24:32 +0200

    UPDATE: user enum role

--- custom_auth/models.py
+++ custom_auth/models.py
@@ -1,20 +1,31 @@
-from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.db import models
 from typing import Self
 import uuid
 
 
+class RoleTypes(models.TextChoices):
+    ADMIN = "ADMIN", "Admin"
+    USER = "USER", "User"
+    AUTH = "AUTH", "Auth"
+
+
 class UserManager(BaseUserManager):
 
-    def create_user(self, username: str, email: str, password: str, role: str) -> Self:
+    def create_user(
+        self, username: str, email: str, password: str, role: RoleTypes
+    ) -> Self:
         if not username:
             raise ValueError("Users must have a username")
         if not email:
             raise ValueError("Users must have an email address")
+        if role not in RoleTypes.values:
+            raise ValueError("Invalid role type")
 
-        user = self.model(username=username, email=self.normalize_email(email))
+        user = self.model(
+            username=username, email=self.normalize_email(email), role=role.value
+        )
         user.set_password(password)
-        user.role = role
 
         user.save(using=self._db)
 
@@ -24,7 +35,7 @@ class UserManager(BaseUserManager):
         if not password:
             raise ValueError("Superusers must have a password")
 
-        user = self.create_user(username, email, password, "ADMIN")
+        user = self.create_user(username, email, password, RoleTypes.ADMIN)
         user.is_admin = True
         user.save(using=self._db)
 
@@ -35,7 +46,11 @@ class User(AbstractBaseUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     username = models.CharField(max_length=255, unique=True)
     email = models.EmailField(max_length=255, unique=True)
-    role = models.CharField(max_length=50)
+    role = models.CharField(
+        max_length=10,
+        choices=RoleTypes,
+        default=RoleTypes.USER,
+    )
     is_active = models.BooleanField(default=False)
     is_admin = models.BooleanField(default=False)
 
@@ -53,8 +68,8 @@ class User(AbstractBaseUser):
 
     @property
     def is_user(self) -> bool:
-        return self.role == "USER"
+        return self.role == RoleTypes.USER
 
     @property
     def is_auth(self) -> bool:
-        return self.role in ["AUTH", "ADMIN", "USER"]
+        return self.role in RoleTypes

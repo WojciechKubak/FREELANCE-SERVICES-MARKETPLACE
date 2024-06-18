13af49733f201ebca75ba7df3576a4455fe9282f
WojciechKubak <wojciechkubak99@gmail.com>
2024-06-18 11:03:01 +0200

    ADD: pytest-django setup

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ ENV PYTHONUNBUFFERED 1
 
 WORKDIR /app
 COPY /Pipfile.lock /Pipfile /app/
-RUN pip install pipenv && pipenv install --system --deploy --ignore-pipfile
+RUN pip install pipenv && pipenv install --system --dev --ignore-pipfile
 COPY . /app
 
 CMD ["gunicorn", "-c", "config/gunicorn/conf.py", "--bind", ":8000", "--chdir", "app", "app.wsgi:application", "--reload"]
--- app/settings/testing.py
+++ app/settings/testing.py
@@ -1,17 +1,21 @@
 from app.settings.base import *
 
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": "db_1",
         "USER": "user",
         "PASSWORD": "user1234",
         "HOST": "db_test",
-        "PORT": "5433",
+        "PORT": "5432",
+        "TEST": {
+            "NAME": "db_1",
+        },
     }
 }
+
 
 PASSWORD_HASHERS = ["django.contrib.auth.hashers.MD5PasswordHasher"]

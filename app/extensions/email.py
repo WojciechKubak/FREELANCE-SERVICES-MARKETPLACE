cd0984e22208b573e07c92c7e7b9dd3d13a13892
WojciechKubak <wojciechkubak99@gmail.com>
2024-06-18 17:18:52 +0200

    FIX: env port type casting

--- app/extensions/email.py
+++ app/extensions/email.py
@@ -1,4 +1,4 @@
-from app.env import env_to_bool, env_to_int
+from app.env import env_to_bool
 import os
 
 
@@ -6,6 +6,6 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 EMAIL_HOST = os.environ.get("EMAIL_HOST")
 EMAIL_USE_TLS = env_to_bool(os.environ.get("EMAIL_USE_TLS"))
 EMAIL_USE_SSL = env_to_bool(os.environ.get("EMAIL_USE_SSL"))
-EMAIL_PORT = env_to_int(os.environ.get("EMAIL_PORT"))
+EMAIL_PORT = os.environ.get("EMAIL_PORT")
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")

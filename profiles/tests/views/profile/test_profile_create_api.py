ddbc463533cff0c122b6e58cd3037bc26c134f10
WojciechKubak <wojciechkubak99@gmail.com>
2024-06-30 19:43:26 +0200

    Add test case for profile create api

--- profiles/tests/views/profile/test_profile_create_api.py
+++ profiles/tests/views/profile/test_profile_create_api.py
@@ -1,4 +1,4 @@
-from common.factories import UserFactory, ProfileDataFactory
+from common.factories import UserFactory, ProfileFactory, ProfileDataFactory
 from profiles.views import ProfileCreateApi
 from profiles.models import Profile
 import pytest
@@ -9,11 +9,22 @@ class TestProfileCreateApi:
     data = ProfileDataFactory()
 
     @pytest.mark.django_db
+    def test_when_user_profile_already_exists(self, auth_request) -> None:
+        profile = ProfileFactory()
+        request = auth_request(
+            user=profile.user, method="POST", url=self.url, data=self.data
+        )
+
+        response = ProfileCreateApi.as_view()(request)
+
+        assert 400 == response.status_code
+
+    @pytest.mark.django_db
     def test_when_profile_created(self, auth_request) -> None:
         user = UserFactory()
         request = auth_request(user=user, method="POST", url=self.url, data=self.data)
 
         response = ProfileCreateApi.as_view()(request)
 
-        assert response.status_code == 201
+        assert 201 == response.status_code
         assert Profile.objects.filter(user=user).exists()
